26d57fdf7841eff912175451ff14e5d87e3b60e6
manolotis <manuel.munoz.sanchez@gmail.com>
2018-12-11 13:13:36 +0100

    Replaced sparseoperations to include contrastive divergence update

--- SET-DBN/cython/sparseoperations.pyx
+++ SET-DBN/cython/sparseoperations.pyx
@@ -3,14 +3,15 @@
 
 cimport numpy as np
 
-def backpropagation_updates_Cython(np.ndarray[np.float64_t,ndim=2] a, np.ndarray[np.float64_t,ndim=2] delta, np.ndarray[int,ndim=1] rows, np.ndarray[int,ndim=1] cols,np.ndarray[np.float64_t,ndim=1] out):
+def contrastive_divergence_updates_Cython(double wDecay, double lr, np.ndarray[np.float64_t,ndim=2] DV, np.ndarray[np.float64_t,ndim=2] DH, np.ndarray[np.float64_t,ndim=2] MV, np.ndarray[np.float64_t,ndim=2] MH, np.ndarray[int,ndim=1] rows, np.ndarray[int,ndim=1] cols,np.ndarray[np.float64_t,ndim=1] out):
     cdef:
         size_t i,j
-        double s
+        double s1,s2
     for i in range (out.shape[0]):
-        s=0
-        for j in range(a.shape[0]):
-            s+=a[j,rows[i]]*delta[j, cols[i]]
-        out[i]=s/a.shape[0]
+        s1=0
+        s2=0
+        for j in range(DV.shape[0]):
+            s1+=DV[j,rows[i]]*DH[j, cols[i]]
+            s2+=MV[j,rows[i]]*MH[j, cols[i]]
+        out[i]+=lr*(s1/DV.shape[0]-s2/DV.shape[0])-wDecay*out[i]
     #return out
-
